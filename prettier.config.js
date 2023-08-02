1c9625181d6dbc29d44dbdab9e8a261cdaef8591
maxime <maxime.deoliveira2509@gmail.com>
2023-08-02 13:55:20 +0200

    feat(linter): setup prettier

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  trailingComma: 'es5',
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  tabWidth: 2,
+  singleAttributePerLine: false,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrder: ['^@/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
