f66900ee24c10f1d863a16ceb0fdae9a012fa0f4
maxime <maxime.deoliveira2509@gmail.com>
2023-08-04 19:12:02 +0200

    chore: use babel for ts paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const tsconfig = require('./tsconfig.json');
+
+const rawAlias = tsconfig.compilerOptions.paths;
+let alias = {};
+
+for (let x in rawAlias) {
+  alias[x.replace('/*', '')] = rawAlias[x].map((p) => p.replace('/*', ''));
+}
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias,
+        },
+      ],
+    ],
   };
 };
